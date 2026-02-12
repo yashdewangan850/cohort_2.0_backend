896d8b59f16dede00e33edf5fb8e5ef495b247c8
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-02-12 18:10:02 +0530

    api GET POST PATCH DELETE

--- DAY-4/src/app.js
+++ DAY-4/src/app.js
@@ -3,17 +3,39 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-notes =[]
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
+notes = [];
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.post("/notes",(req,res)=>{
-    console.log(req.body);
-    notes.push(req.body);
-    console.log(notes);
+// POST notes API
 
-    res.send("This is a post request");
-})
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  console.log(notes);
+
+  res.send("This is a post request");
+});
+
+// GET notes AOI
+
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
+
+// DELETE notes API
+
+app.delete("/notes/:index", (req, res) => {
+  delete notes[req.params.index];
+  res.send("notes deleted successfully");
+});
+
+// PATCH notes API
+
+app.patch("/notes/:index", (req, res) => {
+  notes[req.params.index].description = req.body.description;
+  res.send("notes updated successfully");
+});
 
 module.exports = app;
