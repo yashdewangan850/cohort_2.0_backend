5ad4aa76dab082416183816d836c538a610e2728
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-02-20 10:34:58 +0530

    complete

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../public")))
 //  -POST /appi/notes - Create a new note
 //  -create new notes and store data in mongodb
 //  -req.body - {title, description}
