b0f6e70635413febfe0c87b4da28d9f46cf5bc53
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-02-26 01:40:33 +0530

    add imageKit

--- instaclone/src/controller/post.controller.js
+++ instaclone/src/controller/post.controller.js
@@ -1,7 +1,19 @@
 const postModel = require("../models/post.model")
+const imafeKit = require("@imagekit/nodejs")
+const {toFile}= require("@imagekit/nodejs")
 
+const imagekit = new imafeKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+})
 async function createPostController(req,res){
     console.log(req.body,req.file)
+
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer),"file"),
+        fileName:"Test"
+    })
+
+    res.send(file)
 }
 
 module.exports = {
