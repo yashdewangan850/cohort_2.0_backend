d1ea9dbdc181fdcbcda52b5c88a1376e97da82d8
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-11 23:35:50 +0530

    face dection

---
+++ Face-Detaction/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import FaceExpression from './features/Expression/component/FaceExpression'
+
+function App() {
+
+
+  return (
+    <FaceExpression />
+  )
+}
+
+export default App
