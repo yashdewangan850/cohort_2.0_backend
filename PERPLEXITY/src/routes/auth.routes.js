16cef83766df7cc134782aef8de6da1b7809a6a2
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-14 22:41:47 +0530

    add login

--- PERPLEXITY/src/routes/auth.routes.js
+++ PERPLEXITY/src/routes/auth.routes.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
-import { register} from "../controllers/auth.controller.js";
-import { registerValidator} from "../validators/auth.validator.js";
+import { register , verifyEmail , login} from "../controllers/auth.controller.js";
+import { registerValidator,loginValidator} from "../validators/auth.validator.js";
 
 const authRouter = Router();
 
 
+
+
+//  * @route POST /api/auth/register
+//  * @desc Register a new user
+//  * @access Public
+//  * @body { username, email, password }
+
 authRouter.post("/register", registerValidator, register);
 
 
+/**
+//  * @route POST /api/auth/login
+//  * @desc Login user and return JWT token
+//  * @access Public
+//  * @body { email, password }
+ */
+authRouter.post("/login", loginValidator, login)
+
+
+/**
+//  * @route GET /api/auth/verify-email
+//  * @desc Verify user's email address
+//  * @access Public
+//  * @query { token }
+ */
+
+authRouter.get('/verify-email', verifyEmail)
 
 export default authRouter;
--- PERPLEXITY/src/validators/auth.validator.js
+++ PERPLEXITY/src/validators/auth.validator.js
@@ -27,3 +27,16 @@ export const registerValidator = [
 
     validate
 ];
+
+
+export const loginValidator = [
+    body("email")
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Please provide a valid email"),
+
+    body("password")
+        .notEmpty().withMessage("Password is required"),
+
+    validate
+];
