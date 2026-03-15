27fc05f09f9a3ccad4ee89296faaea848e00379e
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-15 14:58:48 +0530

    add ai model part

---
+++ PERPLEXITY/src/services/ai.service.js
@@ -0,0 +1,12 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash-lite",
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+export async function testAI(){
+    model.invoke("what is Aiin 100 word").then((response)=>{
+        console.log(response.text)
+    })
+}
