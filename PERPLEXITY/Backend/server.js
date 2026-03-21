fdc43ac3de8f07ed027f8f6e66e8c98f5ec4053d
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-21 23:32:43 +0530

    add frontend

--- PERPLEXITY/Backend/server.js
+++ PERPLEXITY/Backend/server.js
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
-import {testAI} from "./src/services/ai.service.js"
+
 
 
 const PORT = process.env.PORT || 3000;
 
-testAI()
 
 connectDB()
     .catch((err) => {
