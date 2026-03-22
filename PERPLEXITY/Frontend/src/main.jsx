b5c83a73ff242db31fdac8ce86542a85fc6eb660
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-22 21:10:55 +0530

    add login part

--- PERPLEXITY/Frontend/src/main.jsx
+++ PERPLEXITY/Frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './app/index.css'
+import App from './app/App.jsx'
+import { store } from './app/app.store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 )
