75e8615880d3abe3dc496aa9986c4111f778b353
yashdewangan850-hub <yashdewangan850@gmail.com>
2026-03-23 18:30:46 +0530

    add

--- PERPLEXITY/Frontend/src/app/app.store.js
+++ PERPLEXITY/Frontend/src/app/app.store.js
@@ -1,10 +1,10 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import authReducer from "../features/auth/auth.slice"
-// import chatReducer from "../features/chat/chat.slice"
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/auth.slice"
+import chatReducer from "../features/chat/chat.slice"
 
-// export const store = configureStore({
-//     reducer: {
-//         auth: authReducer,
-//         chat: chatReducer,
-//     }
-// })
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        chat: chatReducer,
+    }
+})
